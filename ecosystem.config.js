33ca71a5f2f23419039b3bfe8d4fd94c81a0a9b6
teal.yao <lovejs318@gmail.com>
2018-12-25 14:11:01 +0800

    christmax version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     "name": "app",
-    "cwd": "/data/main/source",
+    "cwd": "/data/main/yaowang.name",
     "script": "app.js",
     "instances": 1,
     "exec_mode": "fork",
@@ -11,6 +11,7 @@ module.exports = {
     "log_file": "/dev/null",
     "out_file": "/dev/null",
     "merge_logs": true,
+    "watch": true,
     "log_date_format": "YYYY-MM-DD HH:mm:ss Z",
     "env_production": {
       NODE_ENV: 'production'
@@ -23,7 +24,7 @@ module.exports = {
       host: '35.201.140.94',
       ref: 'origin/master',
       repo: 'git@github.com:teal-front/yaowang.name.git',
-      path: '/data/main/',
+      path: '/data/main/yaowang.name',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     },
   }
